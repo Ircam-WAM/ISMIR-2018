028e83a5bda11c489505d95a9dfa2bfd75daa5dc
geoffroypeeters <geoffroy.peeters@gmail.com>
2017-10-14 18:29:59 +0200

    Update pelican_conf.py

--- pelican_conf.py
+++ pelican_conf.py
@@ -8,7 +8,8 @@ SITEURL = 'http://ismir2018.ircam.fr'
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 #THEME = '/srv/lib/pelican-themes/pelican-striped-html5up'
-THEME = '/srv/lib/pelican-themes/pelican-bootstrap3'
+#THEME = '/srv/lib/pelican-themes/pelican-bootstrap3'
+THEME = '/srv/lib/pelican-themes/flex'
 
 BOOTSTRAP_THEME = 'united'
 # CUSTOM_CSS = 'themes/bootswatch/slate/slate/bootstrap.css'
